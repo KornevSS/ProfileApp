172d5c1eaec0564513c331ae0ef28c640dbe75c6
S.Kornev <post.kornev@gmail.com>
2021-03-01 20:02:33 +0300

    Initial Commit

---
+++ ProfileApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfileApp
+//
+//  Created by Сергей Корнев on 01.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
