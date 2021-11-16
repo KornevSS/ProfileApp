8330232bce734bd2b525b6f8091cd6428ab2471c
S.Kornev <post.kornev@gmail.com>
2021-11-16 09:51:21 +0300

    Adding keyboard events and fixing the search engine

--- ProfileApp/ViewControllers/CompanyListViewController.swift
+++ ProfileApp/ViewControllers/CompanyListViewController.swift
@@ -44,10 +44,11 @@ class CompanyListViewController: UIViewController {
         searchBar.delegate = self
     }
     
-    private func getListOfCompanies(with searchText: String) {
-        NetworkManager.shared.fetchData(with: searchText) { [self] companies in
+    private func getListOfCompanies(with searchText: String, completion: @escaping () -> Void) {
+        NetworkManager.shared.fetchData(with: searchText) { [unowned self] companies in
             guard let companies = companies else { return }
             self.companyList = companies
+            completion()
         }
     }
 
@@ -63,34 +64,38 @@ extension CompanyListViewController: UITableViewDataSource, UITableViewDelegate 
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-      
         var content = cell.defaultContentConfiguration()
         content.text = "\(companyList[indexPath.row].value ?? "") \(companyList[indexPath.row].data?.inn ?? "")"
         content.secondaryText = companyList[indexPath.row].data?.management?.name
         cell.contentConfiguration = content
-
         return cell
     }
     
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        companyCoordsViewController.company = companyList[indexPath.row]
-//
-//        tableView.deselectRow(at: indexPath, animated: true)
-//    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
 }
 
-// MARK: - SearchBar
+// MARK: - SearchBar & work with keyboard
 
 extension CompanyListViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange textSearched: String) {
-        getListOfCompanies(with: textSearched)
-        tableView.reloadData()
+        getListOfCompanies(with: textSearched) {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        searchBar.endEditing(true)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
     }
     
 }
-
 
 // MARK: - Alert
 
